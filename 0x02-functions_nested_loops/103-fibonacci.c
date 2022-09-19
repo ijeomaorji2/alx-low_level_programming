7ecfa40249a8338f650af86f43706455377f7e09
ijeoma orji <ijeomaorji2@gmail.com>
2022-09-19 12:49:52 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,19 +9,18 @@
 
 int main(void)
 {
-    int i = 1, j = 2, total = 0;
-    int k;
+	int i = 1, j = 2, total = 0;
+	int k;
 
-    while (j < 4000000)
-    {
-        if (j % 2 == 0)
-            total += j;
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			total += j;
 
-        k = j;
-        j += i;
-        i = k;
-    }
-    printf("%d\n", total);
-    return (0);
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%d\n", total);
+	return (0);
 }
-
