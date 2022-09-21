52a48ce156a255e346868f226ce3e159690081e1
ijeoma orji <ijeomaorji2@gmail.com>
2022-09-21 08:47:37 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,49 @@
-#include "main.h" 
-  
- /** 
-  * print_times_table - Print the n times table, starting from 0 
-  * @n: The times table to print 
-  * 
-  * Return: Nothing 
-  */ 
- void print_times_table(int n) 
- { 
-         int x, y, mult, one, ten, hund; 
-  
-         x = 0; 
-         if (n >= 0 && n <= 15) 
-         { 
-                 while (x <= n) 
-                 { 
-                         y = 0; 
-                         while (y <= n) 
-                         { 
-                                 mult = x * y; 
-                                 one = mult % 10; 
-                                 ten = mult % 100 / 10; 
-                                 hund = mult / 100; 
-                                 if (hund == 0 && y != 0) 
-                                 { 
-                                         _putchar(' '); 
-                                         if (ten == 0) 
-                                                 _putchar(' '); 
-                                         else 
-                                                 _putchar(ten + '0'); 
-                                 } 
-                                 else if (hund != 0) 
-                                 { 
-                                         _putchar(hund + '0'); 
-                                         _putchar(ten + '0'); 
-                                 } 
-                                 _putchar(one + '0'); 
-                                 if (y != n) 
-                                 { 
-                                         _putchar(','); 
-                                         _putchar(' '); 
-                                 } 
-                                 y++; 
-                         } 
-                         _putchar('\n'); 
-                         x++; 
-                 } 
-         } 
- } 
+#include "main.h"
+
+/**
+* print_times_table - print times tables from zero to n
+* @n: integer
+*/
+
+void print_times_table(int n)
+{
+int a = 0;
+
+if (n >= 0 && n <= 15)
+{
+while (a <= n)
+{
+int b = 1;
+
+_putchar('0');
+while (b <= n)
+{
+int p = a * b;
+
+_putchar(',');
+_putchar(' ');
+if (p < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(p + '0');
+}
+else if (p > 99)
+{
+_putchar((p / 100) + '0');
+_putchar(((p / 10) % 10) + '0');
+_putchar((p % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
+}
+} 
