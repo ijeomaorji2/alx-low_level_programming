d27d7fef9a3f2c2b91e905f13688a6cc79020596
ijeoma orji <ijeomaorji2@gmail.com>
2022-09-21 08:28:17 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,22 +1,20 @@
-#ifndef HOLBERTON_H 
- #define HOLBERTON_H 
+#ifndef MAIN_H 
+ #define MAIN_H 
   
- void positive_or_negative(int i); 
- int largest_number(int a, int b, int c); 
- int convert_day(int month,int day); 
- void print_remaining_days(int month,int day,int year); 
- void print_to_98(int n); 
+ #include <stdio.h> 
+  
  int _isupper(int c); 
  int _isdigit(int c); 
  int mul(int a, int b); 
- void print_numbers(void); 
  int _putchar(char c); 
+ void print_numbers(void); 
  void print_most_numbers(void); 
  void more_numbers(void); 
  void print_line(int n); 
  void print_diagonal(int n); 
  void print_square(int size); 
  void print_triangle(int size); 
+ void num_to_char(unsigned int); 
  void print_number(int n); 
   
- #endif
+ #endif /* MAIN_H */
