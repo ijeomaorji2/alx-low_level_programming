dade2eb9a173ee798f31017bfec623ed56c110df
ijeomaorji2 <104699127+ijeomaorji2@users.noreply.github.com>
2022-09-27 21:33:45 +0100

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,21 @@
-#include "main.h" 
-  
- /** 
-  * print_chessboard - a function that prints the chessboard. 
-  * @a: Double pointer 
-  * Return: 0 
-  */ 
-  
- void print_chessboard(char (*a)[8]) 
- { 
-         unsigned int i, j; 
-  
-         for (i = 0; i < 8; i++) 
-         { 
-                 for (j = 0; j < 8; j++) 
-                         _putchar(a[i][j]); 
-                 _putchar('\n'); 
-         } 
-  
- }
+#include "main.h"
+
+/**
+ * print_chessboard - Prints chessboard
+ *
+ * @a: Double pointer
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
