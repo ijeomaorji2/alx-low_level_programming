b9f73700afa3966ba03f8929e9276ee6724691e8
ijeoma orji <ijeomaorji2@gmail.com>
2022-10-01 10:56:06 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+ #include "main.h" 
+  
+ /** 
+  * print_line - print straight line 
+  * @n: parameter 
+  * Return: 0 
+  */ 
+  
+ void print_line(int n) 
+ { 
+         while (n-- > 0) 
+         { 
+                 _putchar('_'); 
+         } 
+         _putchar('\n'); 
+  
+ }
