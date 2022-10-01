0ef1a40be0bd76a1dd6d22f3458a0187597f881d
ijeoma orji <ijeomaorji2@gmail.com>
2022-10-01 11:09:57 -0700

    Task 13

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+ #include "main.h" 
+  
+ /** 
+  * print_number - print a number 
+  * @n: number to be printed 
+  * Return: 0 
+  */ 
+  
+ void print_number(int n) 
+ { 
+         unsigned int num; 
+  
+         num = n; 
+  
+         if (n < 0) 
+         { 
+                 _putchar('-'); 
+                 num = -n; 
+         } 
+         if  (num / 10) 
+         { 
+                 print_number(num / 10); 
+         } 
+         _putchar(num % 10 + '0'); 
+  
+ }
