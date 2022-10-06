56152b7077ab326ca6cc1132eb91f65bb54e7d1e
ijeoma orji <ijeomaorji2@gmail.com>
2022-10-06 07:29:44 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h" 
+ #include <stdio.h> 
+  
+ /** 
+  * _puts - prints string 
+  * @str: string 
+  * Return: 0 
+  */ 
+  
+ void _puts(char *str) 
+ { 
+         int m; 
+  
+         m = 0; 
+         while (*str != '\0') 
+         { 
+                 _putchar(*str); 
+                 m++; 
+                 str++; 
+         } 
+         _putchar('\n'); 
+  
+ }
