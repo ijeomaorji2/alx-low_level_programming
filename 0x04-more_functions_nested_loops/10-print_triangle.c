c14eb7e088d6640b5df0cf786c7eacb864d90931
ijeomaorji2 <104699127+ijeomaorji2@users.noreply.github.com>
2022-10-06 22:07:44 +0800

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,36 +1,36 @@
-#include <stdio.h> 
- #include "main.h" 
-  
- /** 
-  * print_triangle - a function that prints a triangle 
-  * @size: size parameter of triangle 
-  * Return: 0 
-  */ 
-  
- void print_triangle(int size) 
- { 
-         int inc1, inc2; 
-  
-         if (size > 0) 
-         { 
-                 for (inc1 = 1; inc1 <= size; inc1++) 
-                 { 
-                         for ((inc2 = size - inc1); inc2 > 0; inc2--) 
-                         { 
-                                 _putchar(' '); 
-                         } 
-                         for (inc2 = 0; inc2 < inc1; inc2++) 
-                         { 
-                                 _putchar('#'); 
-                         } 
-  
-                         if (inc1 == size) 
-                         { 
-                                 continue; 
-                         } 
-                         _putchar('\n'); 
-  
-                 } 
-         } 
-         _putchar('\n'); 
- }
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: size parameter of triangle
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 1; inc1 <= size; inc1++)
+		{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
+				_putchar(' ');
+			}
+			for (inc2 = 0; inc2 < inc1; inc2++)
+			{
+				_putchar('#');
+			}
+
+			if (inc1 == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+
+		}
+	}
+	_putchar('\n');
+}
