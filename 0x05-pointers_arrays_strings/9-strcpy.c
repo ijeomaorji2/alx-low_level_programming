13bf35dae4c97212f25894a27f22ea9715651edb
ijeoma orji <ijeomaorji2@gmail.com>
2022-10-06 07:39:27 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h" 
+  
+ /** 
+  * _strcpy - function that copies the string pointed to by src, 
+  * @dest: pointer to string to be copied to. 
+  * @src: pointer to string to be copied. 
+  * Return: 0 
+  */ 
+  
+ char *_strcpy(char *dest, char *src) 
+ { 
+         int j; 
+  
+         for (j = 0; src[j] != '\0'; j++) 
+         { 
+                 dest[j] = src[j]; 
+         } 
+         dest[j] = '\0'; 
+         return (dest); 
+ }
