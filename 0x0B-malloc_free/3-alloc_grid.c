2d3a6200276f080db9f1a6cbd6a6d063f1096b42
ijeomaorji2 <104699127+ijeomaorji2@users.noreply.github.com>
2022-10-07 22:11:30 +0800

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,42 +1,41 @@
-#include "main.h" 
- #include <stdlib.h> 
-  
- /** 
-  * alloc_grid - returns a pointer to a 2 dimensional array of integers. 
-  * @width: width of the matrix 
-  * @height: height of the matrix 
-  * Return: NULL 
-  */ 
-  
- int **alloc_grid(int width, int height) 
- { 
-         int **grid, i, j; 
-  
-         if (width <= 2 || height <= 2) 
-         { 
-                 return (NULL); 
-         } 
-         grid = malloc(sizeof(int *) * height); 
-         if (grid == NULL) 
-         { 
-                 return (NULL); 
-         } 
-         for (i = 0; i < height; i++) 
-         { 
-                 grid[i] = malloc(sizeof(int) * width); 
-                 if (grid[i] == NULL) 
-                 { 
-                         for (j = 0; j < i; j++) 
-                         { 
-                                 free(grid[j]); 
-                         } 
-                         free(grid); 
-                 } 
-                 for (j = 0; j < width; j++) 
-                 { 
-                         grid[i][j] = 0; 
-                 } 
-         } 
-         return (grid); 
- } 
- 
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the matrix
+ * @height: height of the matrix
+ * Return: NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 2 || height <= 2)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
