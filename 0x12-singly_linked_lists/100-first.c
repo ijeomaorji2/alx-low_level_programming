91678acfd4027fcd5c1383d0b47e7ef870d70a55
ijeomaorji2 <104699127+ijeomaorji2@users.noreply.github.com>
2022-10-21 23:27:46 +0800

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
-#include "lists.h" 
- #include <stdio.h> 
-  
- /** 
-  *print_first - prints a string before main 
-  *Return: nothing 
-  */ 
-  
- __attribute__((constructor)) 
- void print_first(void) 
- { 
-         printf("You're beat! and yet, you must allow,\n"); 
-         printf("I bore my house upon my back!\n"); 
- }
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_first - prints a string before main
+ *Return: nothing
+ */
+
+__attribute__((constructor))
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
